602e49087ab4f402e3167b963923765f2c88d2a5
afgabriel <93103593+afgabriel@users.noreply.github.com>
2021-10-25 00:38:32 -0300

    Update fact_orders.sql

--- models/northwind/marts/fact_orders.sql
+++ models/northwind/marts/fact_orders.sql
@@ -22,7 +22,6 @@ with
             orders.order_id	
             , customers.customer_sk as customer_fk
             , employees.employee_sk as employee_fk
-            , shippers.shipper_sk as shipper_fk	
             , orders.ship_region
             , orders.shipped_date
             , orders.ship_country
@@ -36,7 +35,6 @@ with
         from {{ ref('stg_orders') }} orders
         left join employees on orders.employee_id = employees.employee_id
         left join customers on orders.customer_id = customers.customer_id
-        left join shippers on orders.shipper_id = shippers.shipper_id
     )
     , order_details_with_sk as (
         select
@@ -52,8 +50,7 @@ with
         select
             order_details_with_sk.order_id	
             , orders_with_sk.customer_fk
-            , orders_with_sk.employee_fk
-            , orders_with_sk.shipper_fk	
+            , orders_with_sk.employee_fk	
             , orders_with_sk.ship_region
             , orders_with_sk.shipped_date
             , orders_with_sk.ship_country
