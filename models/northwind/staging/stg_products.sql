92c1524a71227aff8082657438169b7d97199dfb
afgabriel <93103593+afgabriel@users.noreply.github.com>
2021-10-25 00:27:21 -0300

    Update stg_products.sql

--- models/northwind/staging/stg_products.sql
+++ models/northwind/staging/stg_products.sql
@@ -1,23 +1,18 @@
 with
-    source as (
+    sources as (
         select
-            
-            product_id
-            , supplier_id
-            , category_id
-            , product_name
-            , unit_price
-            , quantity_per_unit
-            , units_in_stock
-            , units_on_order
-            , reorder_level
-            , 
-                case
-                    when discontinued = 1 then True
-                    else False
-                end as is_discontinued
+           product_id	
+           ,product_name
+           ,supplier_id	
+           ,category_id
+           ,quantity_per_unit	
+           ,unit_price		
+           ,units_in_stock	
+           ,units_on_order		
+           ,reorder_level		
+           ,discontinued	
 
-        from {{ source('erpnorthwind', 'public_products') }}
+        from {{ source('erpnorthwind','public_products') }}
     )
 
-select * from source
+select * from sources
